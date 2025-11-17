69c66d60b1199ae951980cd620ac35a09a317c29
azadeh selahvarzi <79242227+azadeh-selahvarzi@users.noreply.github.com>
2025-11-16 23:25:24 -0500

    Add query to count vendor booth rentals

--- 02_activities/assignments/Cohort_8/assignment1.sql
+++ 02_activities/assignments/Cohort_8/assignment1.sql
@@ -76,6 +76,11 @@ ORDER BY vendor_name, market_date;
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
+SELECT 
+ vendor_id
+,COUNT ( booth_number) as rent
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
