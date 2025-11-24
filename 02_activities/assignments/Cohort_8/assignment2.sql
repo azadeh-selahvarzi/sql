1d631d3e7494c1f756bc5e5a02976806441684c9
azadeh selahvarzi <79242227+azadeh-selahvarzi@users.noreply.github.com>
2025-11-24 06:51:27 -0500

    Add REGEXP filter for product_size in SQL query
    Add a query to filter products by size containing numbers.

--- 02_activities/assignments/Cohort_8/assignment2.sql
+++ 02_activities/assignments/Cohort_8/assignment2.sql
@@ -110,6 +110,9 @@ FROM product;
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
 
+SELECT *
+FROM product
+WHERE product_size REGEXP '[0-9]';
 
 
 -- UNION
