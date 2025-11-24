9bc3bc7849a776ef2e3c8f75612bd2e628834790
azadeh selahvarzi <79242227+azadeh-selahvarzi@users.noreply.github.com>
2025-11-23 21:09:59 -0500

    Add query to extract product descriptions
    Add SQL query to extract product descriptions from product_name

--- 02_activities/assignments/Cohort_8/assignment2.sql
+++ 02_activities/assignments/Cohort_8/assignment2.sql
@@ -95,6 +95,19 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
+SELECT 
+    product_name,
+    CASE 
+        WHEN INSTR(product_name, '-') > 0 THEN
+            LTRIM(RTRIM(
+                SUBSTR(
+                    product_name,
+                    INSTR(product_name, '-') + 1
+			)
+            ))
+        ELSE NULL
+    END AS description
+FROM product;
 
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
