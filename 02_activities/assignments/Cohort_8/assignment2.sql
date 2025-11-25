d80c1a38841cf088c3d6fff9a23ae3100318a076
azadeh selahvarzi <79242227+azadeh-selahvarzi@users.noreply.github.com>
2025-11-24 20:04:01 -0500

    Alter product_units to add current_quantity column
    Add current_quantity column and update it from vendor_inventory.

--- 02_activities/assignments/Cohort_8/assignment2.sql
+++ 02_activities/assignments/Cohort_8/assignment2.sql
@@ -251,6 +251,22 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
+ALTER TABLE product_units
+ADD current_quantity INT;
+
+UPDATE product_units AS pu
+SET current_quantity = COALESCE(
+    (
+        SELECT vi.quantity
+        FROM vendor_inventory AS vi
+        WHERE vi.product_id = pu.product_id
+        ORDER BY vi.rowid DESC
+        LIMIT 1
+    ),
+    0
+);
+
+
 
 
 
